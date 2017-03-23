2a6b96e3609223b011f2d012ccaeabcec7dd44b4
Zach Ward <zach.l.ward6@gmail.com>
2017-03-22 21:24:57 -0400

    uncomment seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# users = User.create!([
-#   { user_name: 'zach', password: 'password' },
-#   { user_name: 'john', password: 'password' },
-#   { user_name: 'rob', password: 'password' },
-#   { user_name: 'allie', password: 'password' },
-#   { user_name: 'bre', password: 'password' },
-#   { user_name: 'charlie', password: 'password' },
-#   { user_name: 'glenn', password: 'password' },
-#   { user_name: 'mary', password: 'password' },
-#   { user_name: 'chris', password: 'password' },
-#   { user_name: 'adam', password: 'password' },
-#   { user_name: 'sarah', password: 'password' }
-# ])
-#
+users = User.create!([
+  { user_name: 'zach', password: 'password' },
+  { user_name: 'john', password: 'password' },
+  { user_name: 'rob', password: 'password' },
+  { user_name: 'allie', password: 'password' },
+  { user_name: 'bre', password: 'password' },
+  { user_name: 'charlie', password: 'password' },
+  { user_name: 'glenn', password: 'password' },
+  { user_name: 'mary', password: 'password' },
+  { user_name: 'chris', password: 'password' },
+  { user_name: 'adam', password: 'password' },
+  { user_name: 'sarah', password: 'password' }
+])
+
 
 cats = Cat.create!([
   { name: 'Hans', birth_date: '1994-10-01', color: 'black', user_id: 1, description: "I'm an ugly cat", sex: 'M'},
   { name: 'Frans', birth_date: '1994-09-02', color: 'white', user_id: 2, description: "I'm a pretty cat", sex: 'M'},
   { name: 'Bronse', birth_date: '1994-08-03', color: 'orange', user_id: 3, description: "I'm a mean cat", sex: 'M'},
   { name: 'Mans', birth_date: '1994-07-04', color: 'brown', user_id: 1, description: "I'm a cute cat", sex: 'M'},
-  { name: 'Jerry', birth_date: '1994-06-05', color: 'black', user_id: 2, description: "I'm a gentle cat", sex: 'M'},
+  { name: 'Jerry', birth_date: '1994-06-05', color 'black', user_id: 2, description: "I'm a gentle cat", sex: 'M'},
   { name: 'Nala', birth_date: '1994-05-06', color: 'white', user_id: 3, description: "I'm a wild cat", sex: 'F'},
   { name: 'Simba', birth_date: '1994-04-07', color: 'orange', user_id: 1, description: "I'm a tom cat", sex: 'M'},
   { name: 'Kitty', birth_date: '1994-03-08', color: 'brown', user_id: 2, description: "I'm a stupid cat", sex: 'F'},
